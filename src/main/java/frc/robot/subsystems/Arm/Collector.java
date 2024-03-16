3531519144a72cd026ae60e34aaf1f2cc92141fd
Chris Rapson <crapson89@gmail.com>
2024-03-16 13:59:27 -0400

    Updating piece sensor to the DIO one

--- src/main/java/frc/robot/subsystems/Arm/Collector.java
+++ src/main/java/frc/robot/subsystems/Arm/Collector.java
@@ -5,17 +5,15 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.CollectorConstants;
 import frc.robot.subsystems.LedManager.LedManagerSubsystem;
-import edu.wpi.first.wpilibj.PWM;
 
 public class Collector  extends SubsystemBase { 
     // LedManagerSubsystem ledManager;
-    //DigitalInput pieceSensor;
-    DutyCycleEncoder pieceSensor;
+    DigitalInput pieceSensor;
+    // DutyCycleEncoder pieceSensor;
     private CANSparkMax collectorMotorController;
 
     public Collector(LedManagerSubsystem ledManager){
@@ -28,29 +26,16 @@ public class Collector  extends SubsystemBase {
         collectorMotorController.setInverted(true);
         collectorMotorController.burnFlash();
 
-        pieceSensor = new DutyCycleEncoder(0);
-        //pieceSensor = new DigitalInput(0);
+        // pieceSensor = new DutyCycleEncoder(0);
+        pieceSensor = new DigitalInput(0);
     }
      
     public boolean hasPiece(){
-
-        if (pieceSensor.getDistance() < 0.04){
-        return true; //pieceSensor.get();
-        }
-
-        else{
-            return false;
-        }
+        return pieceSensor.get();
     }
 
     public void printSensorValue(){
-
-        System.out.println("Connected: " + pieceSensor.isConnected());
-
         System.out.println("Raw Value: " + pieceSensor.get());
-
-        System.out.println("Distance:  " + pieceSensor.getDistance());
-
     }
 
     public void run(){
