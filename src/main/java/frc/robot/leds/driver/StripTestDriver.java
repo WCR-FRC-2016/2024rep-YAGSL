14ae37efabb7bfeea24bd8b37a861ea710e53ce6
Littledog1229 <buildmine202@gmail.com>
2024-03-30 18:13:57 -0400

    Extra Drivers

---
+++ src/main/java/frc/robot/leds/driver/StripTestDriver.java
@@ -0,0 +1,15 @@
+package frc.robot.leds.driver;
+
+import frc.robot.leds.ILedStateDriver;
+import frc.robot.leds.LedInfo;
+import frc.robot.leds.LedWrappr;
+
+public final class StripTestDriver implements ILedStateDriver {
+    @Override
+    public void setLEDS(LedWrappr leds, LedInfo led_info) {
+        leds.StripA.fillAllRGB(255, 0, 0);
+        leds.StripB.fillAllRGB(0, 255, 0);
+        leds.StripC.fillAllRGB(0, 0, 255);
+        leds.StripD.fillAllRGBGradient(0, 0, 0, 255, 255, 255);
+    }
+}
