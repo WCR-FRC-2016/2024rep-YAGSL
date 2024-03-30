949190e865b4c849a21cb7ad7bfa3c3fa87cc465
Greg Schwartz <55564018+Murpful@users.noreply.github.com>
2024-03-30 16:53:20 -0400

    Significant working fixes to the speaker shot.

--- src/main/java/frc/robot/commands/arm/TargetSpeakerCommand.java
+++ src/main/java/frc/robot/commands/arm/TargetSpeakerCommand.java
@@ -3,21 +3,26 @@ package frc.robot.commands.arm;
 import java.lang.reflect.Array;
 
 import edu.wpi.first.apriltag.AprilTag;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
+import frc.robot.leds.LedManager;
 import frc.robot.subsystems.Arm.Arm;
 import frc.robot.utilities.LimelightUtility;
+
 
 public class TargetSpeakerCommand extends Command {
     Arm arm;
     private static final double speakerBaseOffset = 0.991;
-private static final double distanceToAngleFactorClose = 0.062;
-    private static final double distanceToAngleFactorMedium = 0.044;
+private static final double distanceToAngleFactorClose = 0.065;
+    private static final double distanceToAngleFactorMedium = 0.047;
     private static final double mediumDistance = 2;
     private static final double distanceToAngleFactorFar = 0.025;
     private static final double farDistance = 2.7;
-    private static final double horizontalDistanceOffsetFactor = 0.020;
-    private static double [] averageReadsArray = new double [5];
+    private static final double horizontalDistanceOffsetFactorClose = 0.023;
+    private static final double horizontalDistanceOffsetFactorMedium = 0.033;
+    private static double [] averageReadsArrayZ = new double [10];
+    private static double [] averageReadsArrayX = new double [10];
     private int aprilTagReadIndex = 0;
 
 
@@ -34,8 +39,13 @@ private static final double distanceToAngleFactorClose = 0.062;
         var distanceToAprilTag = Math.abs(botpose[2]);
         
         var distanceToAprilTagHorizontal = Math.abs(botpose[0]);
-        var horizontalDistanceOffset = distanceToAprilTagHorizontal / 2.0f * horizontalDistanceOffsetFactor;
-        System.out.println(horizontalDistanceOffset);
+        if(Math.abs(distanceToAprilTagHorizontal) < 1){
+            var horizontalDistanceOffset = distanceToAprilTagHorizontal / 2.0f * horizontalDistanceOffsetFactorClose;
+        }
+        else{
+            var horizontalDistanceOffset = distanceToAprilTagHorizontal / 2.0f * horizontalDistanceOffsetFactorMedium;
+        }
+
     }
 
     @Override
@@ -45,52 +55,70 @@ private static final double distanceToAngleFactorClose = 0.062;
         var botpose = LimelightUtility.getBotPos();
 
         var distanceToAprilTag = Math.abs(botpose[2]);
+        var distanceToAprilTagHorizontal = Math.abs(botpose[0]);;
 
+        boolean checkSight = botpose[2] < 0;
 
-        averageReadsArray[aprilTagReadIndex] = distanceToAprilTag;
+        double horizontalDistanceOffset;
+
+        if(checkSight){
+            LedManager.setState("Resting");
+
+            averageReadsArrayZ[aprilTagReadIndex] = distanceToAprilTag;
+            averageReadsArrayX[aprilTagReadIndex] = distanceToAprilTagHorizontal;
+            aprilTagReadIndex = (aprilTagReadIndex + 1)%averageReadsArrayZ.length;
+        } else {
+            LedManager.setState("NoLimelight");
+        }
         
+        double averageDistanceToAprilTagZ = averageArray(averageReadsArrayZ);
+        double averageDistanceToAprilTagX = averageArray(averageReadsArrayX);
 
-
-        if (botpose.length == 0) {
-            System.out.println("couldn't find the apriltag");
-        }
-        if (distanceToAprilTag < mediumDistance) {
-            System.out.println("close");
-        }
-        if (distanceToAprilTag >= mediumDistance && distanceToAprilTag < farDistance) {
-            System.out.println("medium");
-        }
-        if (distanceToAprilTag >= farDistance) {
-            System.out.println("far");
-        }
-        if (distanceToAprilTag < mediumDistance) {
+        // if (botpose.length == 0) {
+        //     System.out.println("couldn't find the apriltag");
+        // }
+        // if (distanceToAprilTag < mediumDistance) {
+        //     System.out.println("close");
+        // }
+        // if (distanceToAprilTag >= mediumDistance && distanceToAprilTag < farDistance) {
+        //     System.out.println("medium");
+        // }
+        // if (distanceToAprilTag >= farDistance) {
+        //     System.out.println("far");
+        // }
+        if (averageDistanceToAprilTagZ < mediumDistance) {
             distanceToAngleFactorActual = distanceToAngleFactorClose;
             // System.out.println("close");
 
         }
-        if (distanceToAprilTag >= mediumDistance && distanceToAprilTag < farDistance) {
+        if (averageDistanceToAprilTagZ >= mediumDistance && distanceToAprilTag < farDistance) {
             distanceToAngleFactorActual = distanceToAngleFactorMedium;
             // System.out.println("medium");
 
         }
-        if (distanceToAprilTag >= farDistance) {
+        if (averageDistanceToAprilTagZ >= farDistance) {
             distanceToAngleFactorActual = distanceToAngleFactorFar;
             // System.out.println("far");
 
         }
-        var distanceToAprilTagHorizontal = Math.abs(botpose[0]);
-        var horizontalDistanceOffset = distanceToAprilTagHorizontal / 2.0f * horizontalDistanceOffsetFactor;
+
+        if(Math.abs(averageDistanceToAprilTagX) < 1){
+            horizontalDistanceOffset = averageDistanceToAprilTagX / 2.0f * horizontalDistanceOffsetFactorClose;
+        }
+        else{
+            horizontalDistanceOffset = averageDistanceToAprilTagX / 2.0f * horizontalDistanceOffsetFactorMedium;
+        }
+
         // ledManagerSubsystem.setState(6);
 
         // var desiredAngle = Constants.RobotDemensions.ArmDipLimit
         //         - ((distanceToAprilTag - speakerBaseOffset) * distanceToAngleFactorActual + horizontalDistanceOffset);
         // System.out.println(horizontalDistanceOffset);
          var desiredAngle = Constants.RobotDemensions.ArmDipLimit
-                - ((averageArray(averageReadsArray) - speakerBaseOffset) * distanceToAngleFactorActual + horizontalDistanceOffset);
+                - ((averageArray(averageReadsArrayZ) - speakerBaseOffset) * distanceToAngleFactorActual + horizontalDistanceOffset);
         
-        
+        System.out.println(desiredAngle);
         arm.setAngle(desiredAngle);
-        aprilTagReadIndex = (aprilTagReadIndex + 1)%averageReadsArray.length;
     }
 
     @Override
@@ -102,6 +130,7 @@ private static final double distanceToAngleFactorClose = 0.062;
         double sum = 0;
         double previousValue = 0;
         for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
             if(array[i] == 0){
                 sum += previousValue;
             }
@@ -109,7 +138,9 @@ private static final double distanceToAngleFactorClose = 0.062;
                 sum += array[i];
                 previousValue = array[i];
             }
+            
         }
+        System.out.println();
         return sum/array.length;
     }
 
