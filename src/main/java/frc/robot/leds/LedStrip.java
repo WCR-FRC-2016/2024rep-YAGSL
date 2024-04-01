73a9cb53e9b9e833c9fc5becfa1a866997d08c46
Littledog1229 <buildmine202@gmail.com>
2024-03-31 21:28:54 -0400

    LedManager Priority System, Sandboxed ILedStateManager calls, LedStrip IndexOutOfBounds bugfix

--- src/main/java/frc/robot/leds/LedStrip.java
+++ src/main/java/frc/robot/leds/LedStrip.java
@@ -10,12 +10,12 @@ public final class LedStrip {
     public void setHSV(int led, int h, int s, int v) { owner.setHSV(led + LEDStart, h, s, v); }
 
     public void fillRangeRGB(int start_led, int end_led, int r, int g, int b) { owner.fillRangeRGB(start_led + LEDStart, end_led + LEDStart, r, g, b); }
-    public void fillCountRGB(int start_led, int count, int r, int g, int b) { fillRangeRGB(start_led + LEDStart, start_led + count + LEDStart, r, g, b); }
-    public void fillAllRGB(int r, int g, int b) { fillRangeRGB(LEDStart, LEDStart + LEDCount, r, g, b);}
+    public void fillCountRGB(int start_led, int count, int r, int g, int b) { owner.fillRangeRGB(start_led + LEDStart, start_led + count + LEDStart, r, g, b); }
+    public void fillAllRGB(int r, int g, int b) { owner.fillRangeRGB(LEDStart, LEDStart + LEDCount, r, g, b);}
 
     public void fillRangeRGBGradient(int start_led, int end_led, int start_r, int start_g, int start_b, int end_r, int end_g, int end_b) { owner.fillRangeRGBGradient(start_led + LEDStart, end_led + LEDStart, start_r, start_g, start_b, end_r, end_g, end_b);}
-    public void fillCountRGBGradient(int start_led, int count, int start_r, int start_g, int start_b, int end_r, int end_g, int end_b) { fillRangeRGBGradient(start_led + LEDStart, start_led + count + LEDStart, start_r, start_g, start_b, end_r, end_g, end_b); }
-    public void fillAllRGBGradient(int start_r, int start_g, int start_b, int end_r, int end_g, int end_b) { fillRangeRGBGradient(LEDStart, LEDStart + LEDCount, start_r, start_g, start_b, end_r, end_g, end_b); }
+    public void fillCountRGBGradient(int start_led, int count, int start_r, int start_g, int start_b, int end_r, int end_g, int end_b) { owner.fillRangeRGBGradient(start_led + LEDStart, start_led + count + LEDStart, start_r, start_g, start_b, end_r, end_g, end_b); }
+    public void fillAllRGBGradient(int start_r, int start_g, int start_b, int end_r, int end_g, int end_b) { owner.fillRangeRGBGradient(LEDStart, LEDStart + LEDCount, start_r, start_g, start_b, end_r, end_g, end_b); }
 
     protected LedStrip(LedWrappr owner, int start_index, int count) {
         this.owner = owner;
